5d69793f3f601e9474c0e6bc172bf0bdf2f21501
Frankie Primerano <frankieprimerano@gmail.com>
2019-05-09 23:39:02 -0400

    Created header file for dstring. Dynamic string library that will make my life so much easier.

---
+++ spear/dstring.h
@@ -0,0 +1,17 @@
+#ifndef H_DSTRING
+#define H_DSTRING
+
+typedef struct dstring {
+    int length;
+    char *text;
+} dstring;
+
+dstring dappend(dstring input, char *character); // Appends a string to the end of the dstring
+dstring dstrip(dstring input, char character); // Strips a character from the string
+dstring dsplit(dstring input, char at); // Splits string at character
+dstring dtrim(dstring input); // Trims whitespace and new line characters from beginning and end of dstring
+dstring dreverse(dstring input); // Reverses a dstring
+dstring dcreate(char *initial); // Creates and returns a new dstring
+dstring dfree(dstring string); // Frees a dstring's memory
+
+#endif
