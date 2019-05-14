40db85dbf0d05566809e222977426210ffaa785c
Frankie Primerano <frankieprimerano@gmail.com>
2019-05-14 13:41:46 -0400

    Added arrays to dstring library

--- fist/dstring.h
+++ fist/dstring.h
@@ -6,6 +6,11 @@ typedef struct dstring {
     char *text;
 } dstring;
 
+typedef struct dstringa {
+    int length;
+    char *dstring;
+} dstringa;
+
 dstring dappendc(dstring input, char character); // Apppend single char to string
 dstring dappend(dstring input, char *characters); // Appends a string to the end of the dstring
 dstring dstrip(dstring input, char character); // Strips a character from the string
@@ -16,8 +21,18 @@ dstring dreplace(dstring input, char there, char with); // Replaces char with an
 int dindexof(dstring input, char character); // Returns the index of a character or -1
 dstring dcreate(char *initial); // Creates and returns a new dstring
 dstring dempty(); // Creates an empty dstring
-dstring dsplice(dstring input, int start, int end); // Splices a string between to indices
+dstring dsubstr(dstring input, int start, int end); // Returns the string between to indices of the input dstring
 int dcount(dstring input, char character); // Count occurances of a character in the dstring
 int dfree(dstring string); // Frees a dstring's memory
+
+// List of dstrings 
+
+dstringa dcreatea(); // Create empty array of dstrings
+dstringa dpush(dstringa array, dstring input); // Push dstring to list of dstrings
+dstringa dremove(dstringa array, dstring input); // Remove item from dstring array
+dstringa dpop(dstringa array); // Pop from stack
+int dindexofa(dstringa array, dstring input); // Find index of dstring
+dstringa dset(dstringa array, int index, dstring with); // Replace index in dstring array with another dstring
+int dfreea(dstringa array);
 
 #endif
