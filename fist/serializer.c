23a72d9cf9123cba70a07d90e8b334d69cba0f77
Frankie Primerano <frankieprimerano@gmail.com>
2019-07-03 02:14:28 -0400

    Adding compression along side decompressed version. Writing compressed to its own file for testing purposes.

--- fist/serializer.c
+++ fist/serializer.c
@@ -5,11 +5,13 @@
 #include "stdint.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include <string.h>
 
 void sdump(hashmap *hmap) {
     // Write size of hashmap to file. (# keys)
 
     FILE *dump = fopen("fist.db", "wb");
+    FILE *dump_compressed = fopen("fist.db.compressed", "wb");
 
     uint32_t num_indices = 0;
 
@@ -19,6 +21,9 @@ void sdump(hashmap *hmap) {
         if(on.length > 0)
             num_indices++;
     }
+    unsigned int current_size = sizeof(num_indices);
+    void *buffer = malloc(current_size);
+    memcpy(buffer, &num_indices, sizeof(num_indices));
 
     fwrite(&num_indices, sizeof(num_indices), 1, dump);
     // Iterate through hashmap and write key and array of values to file
@@ -30,22 +35,45 @@ void sdump(hashmap *hmap) {
                 keyval object = on.maps[key];
                 uint32_t length = object.key.length;
                 // Writes key length and key name to db file
+
+                current_size += sizeof(length);
+                buffer = realloc(buffer, current_size);
+                memcpy(buffer, &length, sizeof(length));
                 fwrite(&length, sizeof(length), 1, dump);
+
+                current_size += object.key.length;
+                buffer = realloc(buffer, current_size);
+                memcpy(buffer, dtext(object.key), object.key.length);
                 fwrite(dtext(object.key), object.key.length, 1, dump);
 
                 uint32_t num_values = object.values.length;
                 // Writes number of values associated with key to db file
+                current_size += sizeof(num_values);
+                buffer = realloc(buffer, current_size);
+                memcpy(buffer, &num_values, sizeof(num_values));
                 fwrite(&num_values, sizeof(num_values), 1, dump);
                 for(int value = 0; value < object.values.length; value++) {
                     // Writes value to db file
                     dstring value_on = object.values.values[value];
                     uint32_t val_length = value_on.length;
+
+                    current_size += sizeof(val_length);
+                    buffer = realloc(buffer, current_size);
+                    memcpy(buffer, &val_length, sizeof(val_length));
                     fwrite(&val_length, sizeof(val_length), 1, dump);
+
+                    current_size += sizeof(value_on.length);
+                    buffer = realloc(buffer, current_size);
+                    memcpy(buffer, dtext(value_on), value_on.length);
                     fwrite(dtext(value_on), value_on.length, 1, dump);
                 }
             }
         }
     }
+
+    printf("%d\n", current_size);
+    fwrite(buffer, current_size, 1, dump_compressed);
+    fclose(dump_compressed);
     fclose(dump);
 }
 
